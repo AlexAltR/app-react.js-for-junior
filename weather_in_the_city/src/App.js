3e3e3d721affe0e583bd5696231cc7008a432a68
AlexAltR <trud.resurs.altay@gmail.com>
2021-08-08 19:07:14 +0700

    Added the asynchronous 'getting Weather' method to the App class, which takes data from the site and converts the data to json format

--- weather_in_the_city/src/App.js
+++ weather_in_the_city/src/App.js
@@ -3,8 +3,16 @@ import Info from "./components/info"; // Импортируем класс Info 
 import Form from "./components/form"; // Импортируем класс Form из файла form.js
 import Weather from "./components/weather_info"; // Импортируем класс Weather из файла weather.js
 
+const API_KEY = "a0ec0b4fd3e3e3eff8952287564ef9ae";
 
 class App extends React.Component { // Создаем класс App, который унаследует все от React.Component
+
+  gettingWeather = async () => { 
+    const api_url = await fetch(`api.openweathermap.org/data/2.5/weather?q=Kiev,ua&appid=${API_KEY}&units=metric`); // fetch() - позволяет прочитать URL адрес и получить с него данные
+    const data = await api_url.json(); // Преобразуем полученные данные в json формат
+    console.log(data);
+  }
+
   render() { //Служит для вывода элементов в браузер
     return(
       <div>
